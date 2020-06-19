665f9ef5249d3a280f17aaec854c0425b84add4f
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-06-19 16:05:00 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="squirrel-lps",
+    version="0.0.1",
+    author="Wesley Chalmers",
+    author_email="breq@breq.dev",
+    description="Local positioning system for ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Breq16/squirrel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
