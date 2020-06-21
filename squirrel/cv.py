838ee8f8e0fa7eb3c44111982fb6fffa54c53925
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-06-20 20:26:23 -0400

    go back to USB camera

--- squirrel/cv.py
+++ squirrel/cv.py
@@ -1,26 +1,18 @@
-import io
+import time
 
-from PIL import Image
-import numpy as np
 import zbar
 import cv2
-import picamera
 
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-camera.sharpness = 100
-#camera.framerate = 1
-#camera.shutter_speed = 100
-camera_stream = io.BytesIO()
+camera = cv2.VideoCapture(0)
 
 cv2.namedWindow("Squirrel LPS")
 
 scanner = zbar.Scanner()
 
+last_frame = time.time()
+
 while True:
-    image = np.empty((320 * 240 * 3,), dtype=np.uint8)
-    camera.capture(image, "bgr")
-    image = image.reshape((240, 320, 3))
+    code, image = camera.read()
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -32,9 +24,17 @@ while True:
             for point in result.position:
                 cv2.circle(image, point, 10, (0, 0, 255), 2)
 
-    cv2.imshow("Squirrel LPS", image)
+    new_time = time.time()
+    frame_time = new_time - last_frame
+    last_frame = new_time
+
+    cv2.putText(image, f"FPS: {int(1/frame_time)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0, 255), 3)
+
+    preview = cv2.resize(image, (640, 480))
+    cv2.imshow("Squirrel LPS", preview)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+camera.release()
 cv2.destroyAllWindows()
